96376a5dc9114e4d920905c63f0742c20cdbeac3
Mantas <sirexas@gmail.com>
2017-05-10 14:05:41 +0300

    Test case when reduce function returns scalar value

--- qvarnmr/worker.py
+++ qvarnmr/worker.py
@@ -15,56 +15,65 @@ def get_arg(context, value):
         return value
 
 
-def process_reduce(qvarn, source_resource_type, key, reducers):
-    context = {
-        'qvarn': qvarn,
-        'source_resource_type': source_resource_type,
-    }
-    for target, reducer in reducers:
-        resources = qvarn.search(source_resource_type, _mr_key=key)
-        args = [get_arg(context, a) for a in reducer['args']]
-        value = reducer['func'](resources, *args)
-        if isinstance(value, dict):
-            value['_mr_value'] = None
-        else:
-            value = {'_mr_value': value}
-        value['_mr_key'] = key
-        resource = qvarn.search_one(target, _mr_key=key, default=None)
-        if resource is None:
-            qvarn.create(target, value)
-        else:
-            qvarn.update(target, resource['id'], value)
-
-
-def process_map(qvarn, source_resource_type, notification, mappers):
+def process_map(qvarn, source_resource_type, notification, resource, mappers):
     context = {
         'qvarn': qvarn,
         'source_resource_type': source_resource_type,
     }
     if notification['resource_change'] == 'created':
-        resource = qvarn.get(source_resource_type, notification['resource_id'])
-        for target, mapper in mappers:
+        for target_resource_type, mapper in mappers:
             args = [get_arg(context, a) for a in mapper['args']]
             for key, value in mapper['func'](resource, *args):
                 value = value or {}
                 value['_mr_key'] = key
                 value['_mr_source_id'] = resource['id']
                 value['_mr_source_type'] = source_resource_type
-                value = qvarn.create(target, value)
-                yield target, key, value
+                value = qvarn.create(target_resource_type, value)
+                yield target_resource_type, key, value
     else:
         raise ValueError('Unknown resource change type: %r' % notification['resource_change'])
+
+
+def process_reduce(qvarn, source_resource_type, key, reducers):
+    context = {
+        'qvarn': qvarn,
+        'source_resource_type': source_resource_type,
+    }
+    for target_resource_type, reducer in reducers:
+        # Get all resources by given key
+        resources = qvarn.search(source_resource_type, _mr_key=key)
+
+        # Call reduce function for all resources matching key
+        args = [get_arg(context, a) for a in reducer['args']]
+        value = reducer['func'](resources, *args)
+
+        # If reduce function returns non-dict value, store it to _mr_value
+        if isinstance(value, dict):
+            value['_mr_value'] = None
+        else:
+            value = {'_mr_value': value}
+
+        # Also store _mr_key in order to be able to update this key with new
+        # value, once one of the source resources will be changed.
+        value['_mr_key'] = key
+
+        # Save reduced value to the target resource type.
+        resource = qvarn.search_one(target_resource_type, _mr_key=key, default=None)
+        if resource is None:
+            qvarn.create(target_resource_type, value)
+        else:
+            qvarn.update(target_resource_type, resource['id'], value)
 
 
 def process(qvarn, listeners, config):
     mappers = defaultdict(list)
     reducers = defaultdict(list)
-    for target, handlers in config.items():
+    for target_resource_type, handlers in config.items():
         for handler in handlers:
             if handler['type'] == 'map':
-                mappers[handler['source']].append((target, handler))
+                mappers[handler['source']].append((target_resource_type, handler))
             elif handler['type'] == 'reduce':
-                reducers[handler['source']].append((target, handler))
+                reducers[handler['source']].append((target_resource_type, handler))
             else:
                 raise ValueError('Unknown map/reduce type, it should be map or reduce, got: %r' % handler['type'])
 
@@ -72,6 +81,14 @@ def process(qvarn, listeners, config):
         notifications = qvarn.get_list(resource_type + '/listeners/' + listener['id'] + '/notifications')
         for notification in notifications:
             notification = qvarn.get(resource_type + '/listeners/' + listener['id'] + '/notifications', notification)
-            for target, key, value in process_map(qvarn, resource_type, notification, mappers[resource_type]):
-                process_reduce(qvarn, target, key, reducers[target])
+            resource = qvarn.get(resource_type, notification['resource_id'])
+
+            # Process map functions.
+            for target_resource_type, key, value in process_map(qvarn, resource_type, notification, resource, mappers[resource_type]):
+                # Process all touched keys with reduce functions.
+                # That means, there is no need to listen notifications of map
+                # target resource types.
+                process_reduce(qvarn, target_resource_type, key, reducers[target_resource_type])
+
+            # Consider this change to be done.
             qvarn.delete(resource_type + '/listeners/' + listener['id'] + '/notifications', notification)
--- tests/test_mapreduce.py
+++ tests/test_mapreduce.py
@@ -126,3 +126,85 @@ def test_mapreduce(pretender, qvarn):
         'org_id': org['id'],
         'report_id': reports[1]['id'],
     }
+
+
+def test_reduce_scalar_value(pretender, qvarn):
+    pretender.add_resource_types({
+        'reports_counts__map': {
+            'path': '/reports_counts__map',
+            'type': 'reports_count__map',
+            'versions': [
+                {
+                    'version': 'v1',
+                    'prototype': {
+                        'id': '',
+                        'type': '',
+                        'revision': '',
+                        '_mr_key': '',
+                        '_mr_source_id': '',
+                        '_mr_source_type': '',
+                    },
+                },
+            ],
+        },
+        'reports_counts': {
+            'path': '/reports_counts',
+            'type': 'reports_count',
+            'versions': [
+                {
+                    'version': 'v1',
+                    'prototype': {
+                        'id': '',
+                        'type': '',
+                        'revision': '',
+                        '_mr_key': '',
+                        '_mr_value': 0,
+                    },
+                },
+            ],
+        },
+    })
+
+    reports_listener = qvarn.create('reports/listeners', {
+        'notify_of_new': True,
+        'listen_on_all': True,
+    })
+
+    listeners = [
+        ('reports', reports_listener),
+    ]
+
+    org = qvarn.create('orgs', {'names': ['Orgtra']})
+    reports = [
+        qvarn.create('reports', {'org': org['id']}),
+        qvarn.create('reports', {'org': org['id']}),
+        qvarn.create('reports', {'org': org['id']}),
+    ]
+
+    process(qvarn, listeners, {
+        'reports_counts__map': [
+            {
+                'source': 'reports',
+                'type': 'map',
+                'func': ref,
+                'args': ['org'],
+            },
+        ],
+        'reports_counts': [
+            {
+                'source': 'reports_counts__map',
+                'type': 'reduce',
+                'func': len,
+                'args': [],
+            },
+        ]
+    })
+
+    reduced = qvarn.get_list('reports_counts')
+    assert len(reduced) == 1
+    reduced = qvarn.get('reports_counts', reduced[0])
+    assert cleaned(reduced) == {
+        'type': 'reports_count',
+        '_mr_key': org['id'],
+        '_mr_value': len(reports),
+    }
