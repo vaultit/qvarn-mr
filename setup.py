f5908c90f82fe32388c81cab1d795e8bb9bba4cf
Mantas <sirexas@gmail.com>
2017-06-19 17:18:51 +0300

    Added licence and debian package (work in progress)

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='qvarn-mr',
     version='0.1.1',
+    license="LGPL",
     packages=find_packages('.'),
     include_package_data=True,
     install_requires=[
