5ab77983b436a92e518f109cfe3dad0a1d445dd4
Justas Trimailovas <trymas@pov.lt>
2017-09-04 18:59:19 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qvarn-mr',
-    version='0.1.4',
+    version='0.1.5',
     license="LGPL",
     packages=find_packages('.'),
     include_package_data=True,
