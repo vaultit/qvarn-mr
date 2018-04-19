1402ee60695e9579dcf35026acf4b0338a863554
Marius Gedminas <marius@gedmin.as>
2018-04-19 16:47:13 +0300

    Back to development: 0.1.11

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='qvarn-mr',
-    version='0.1.10',
+    version='0.1.11.dev0',
     license="LGPL",
     packages=find_packages('.'),
     include_package_data=True,
